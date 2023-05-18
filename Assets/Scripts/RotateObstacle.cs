fd63f1a6777a3e3f3e359875c12460066c5b9e5a
David Brey <dbrey@ucm.es>
2023-05-18 13:50:00 +0200

    Mapa 3 creado

---
+++ Assets/Scripts/RotateObstacle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObstacle : MonoBehaviour
+{
+    public float rotateVel;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, Time.deltaTime * rotateVel, 0.0f);
+
+        if(transform.rotation.y >= 360 || transform.rotation.y <= -360)
+        {
+            Vector3 newRot = new Vector3(0, 0, 0);
+            transform.rotation = Quaternion.Euler(newRot);
+        }
+    }
+}
